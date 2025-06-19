1ce0717cc0b75ed9d47c1b71c3ad7b44b5fa839b
Jesús Izquierdo <jesus.izquierdocubas@gmail.com>
2025-06-19 02:23:21 +0200

    Fix linter errors

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,5 +3,5 @@ from flask import Blueprint
 health_bp = Blueprint("Health", __name__)
 palindromes_bp = Blueprint("Palindromes", __name__)
 
-from . import health  # noqa: F401
-from . import palindromes  # noqa: F401
+from . import health  # noqa: F401, E402
+from . import palindromes  # noqa: F401, E402
